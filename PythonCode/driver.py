e69465500811cec3e97b04899a5817d3429b7771
jman793 <jmarz793@yahoo.com>
2018-10-27 00:12:37 -0500

    Updated to add a basic grid and print funciton

--- PythonCode/driver.py
+++ PythonCode/driver.py
@@ -1,1 +1,23 @@
 #this file will be the driver program used for all three of the algorithms proposed
+
+
+N=10
+#This will define a grid of size NxN
+#The grid will be defined as follows 1 a valid square, 0 an invalid square, 2 the final square
+#For now the input will be non-random
+Grid=[[1 for i in range(N)]for c in range(N)]
+Grid[N-1][N-1]=2
+Grid[N-5][N-2]=0
+Grid[N-7][N-4]=0
+Grid[N-1][N-1]=0
+Grid[N-9][N-8]=0
+Grid[0][N-2]=0
+Grid[N-5][N-6]=0
+Grid[N-4][N-9]=0
+Grid[N-3][N-4]=0
+#This function will print out the grid in a neat fashion
+def print_grid(G):
+    for i in range(len(G)):
+        print G[i]
+
+print_grid(Grid)
